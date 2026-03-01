93145659d6a507418383f53f038db28dde1705e5
Legends-Land <caandrews18@gmail.com>
2026-02-28 19:51:00 -0500

    Created useState for input values. Const handleEvent added as well.

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -7,6 +7,23 @@ import { useEffect, useState} from "react";
 
 function Home() {
 const [host, setHost] = useState([]);
+const [value, setValue] = useState({
+  email: "",
+  password: "",
+});
+
+const handleEvent = (e) =>{
+ setValue({...value, [e.target.name]: e.target.value})
+ e.preventDefault();
+ console.log("Name", e.target.name)
+ console.log("Value", e.target.value)
+};
+
+const handleSubmit = () =>{
+  axios.post("http://localhost:3002/auth/login"),
+  value
+}
+
 
 useEffect(() =>{
   axios.get("http://localhost:3002/host")
@@ -27,6 +44,30 @@ useEffect(() =>{
        </div>
      ))}
     </div>
+    <input 
+    name="email" 
+    placeholder="email" 
+    type="email"
+    onChange={handleEvent}
+    />
+
+    <input 
+    name="password" 
+    placeholder="password" 
+    type=""
+    onChange={handleEvent}
+    />
+
+    {/* <input 
+    name="username" 
+    placeholder="user name" 
+    type=""
+    onChange={handleEvent}
+    /> */}
+    <button
+    onSubmit={handleSubmit}
+    type="submit"
+    >Submit</button>
     </>
     
   )
