ab43d999e53c7c22b69723abd306a9b57cd34a52
Legends-Land <caandrews18@gmail.com>
2026-03-07 17:29:59 -0500

    Res POST data is now being saved in the DB as a user. Page Navigation set as well.

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,31 +1,35 @@
-import React from "react";
-import { Navigate, Link} from "react-router-dom";
+import React, { use } from "react";
+import { useNavigate, Link} from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 //Creating a function be ran when a user logs in
-const Login = ()=> {
+
+function Login () {
+  const navigate = useNavigate();
   //useState is allowing to update the values that are placed
   //in the input section
-const [values, setValues] = useState({
-  email: "",
-  password: "",
-});
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    role: "GUEST"
+  });
+  
+  //Created a function to grab the values from the input and store them
+  const handleEvent = (e) =>{
+    setValues({...values, [e.target.name]: e.target.value})
+    
+    
+  }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const respone = await axios.post(`${import.meta.env.VITE_SERVER_URL}/auth/login`,values)
+      console.log('Login Complete')
+      navigate("/home")
+  
 
-//Created a function to grab the values from the input and store them
-const handleEvent = (e) =>{
-setValues({...values, [e.target.name]: e.target.values})
-e.preventDefault();
-console.log("Name", e.target.name)
-console.log("Email", e.target.email)
-}
- 
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  try {
-    const respone = await axios.post("http://localhost:3002/auth/login")
-    console.log('Login Complete')
-    Navigate("/home")
   } catch(err){
     console.log("Not able to log in")
   }
@@ -35,27 +39,52 @@ const handleSubmit = async (e) => {
 
   return(
     <>
+    <form>
     <h2>Login Page</h2>
     <input 
-    name=""
+    name="email"
     placeholder="email"
     type = ""
     onChange={handleEvent}
     />
     <input 
-    name=""
+    name="password"
     placeholder= "password" 
     type = ""
     onChange={handleEvent}
     />
+
+    <label>
+            <input
+          type="radio"
+          name="role"
+          value="GUEST"
+          checked={values.role === "GUEST"}
+          onChange={handleEvent}
+          /> Guest
+          </label>
+
+          <label>
+            <input
+          type="radio"
+          name="role"
+          value="HOST"
+          checked={values.role === "HOST"}
+          onChange={handleEvent}
+          /> Host
+          </label>
+
     <button
     onClick={handleSubmit}
     >Login</button>
-    
-    <button>Register</button>
 
-    <Link to = "/home">Register</Link>
+    <button>Register</button>
+    
+
+    <Link to = "/register">Register Here</Link>
+</form>
     </>
+
   )
 };
 export default Login;
