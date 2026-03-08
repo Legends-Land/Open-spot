329bef9c1b363d2171588a9e9ce8f2fdb18943d0
Legends-Land <caandrews18@gmail.com>
2026-03-08 16:41:03 -0400

    Added first name and last name input to capture host name after log in.

--- client/src/Pages/HostDashboard.jsx
+++ client/src/Pages/HostDashboard.jsx
@@ -1,7 +1,8 @@
 import React from "react"
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import SideBar from "../Components/sidebar";
 import '../styles/hostDashboard.css'
+import axios from "axios";
 
 // function  Host(){
 // return(
@@ -14,14 +15,56 @@ import '../styles/hostDashboard.css'
 
 
 const Host = () => {
-  const [count, setCount] = useState(0);
+  const [host, setHost] = useState({
+    fistName:"",
+    lastName: ""
+  })
 
-  const handleClick = () => {
-    setCount(count + 1);
-  };
+  const handleEvent = (e) =>{
+    setHost({...host, [e.target.name]: e.target.host})
+    console.log("Name", e.target.name)
+    console.log("Value", e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+    try {
+      const response = await axios.post("http://localhost:3002/host",host)
+    } catch (err) {
+      console.error ("Full Error")
+    }
+  }
+
+  // useEffect(()=> {
+  //   axios.get( `${VITE_SERVER_URL}/host`)
+  // })
 
   return (
     <div>
+      
+      <h2>Host Dashboard</h2>
+      <form>
+        <input
+        type=""
+        name="firstName"
+        onChange={handleEvent}
+        placeholder="First Name"
+        ></input>
+
+        <input
+        type=""
+        name="lastName"
+        onChange={handleEvent}
+        placeholder="Last Name"
+        ></input>
+
+        <button
+        onClick={handleSubmit}
+        >
+          Submit
+        </button>
+
+
+      </form>
       <SideBar/>
     </div>
   );
