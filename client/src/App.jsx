549966023500671c8da419f67ad8540370e861cf
Legends-Land <caandrews18@gmail.com>
2026-03-10 21:01:03 -0400

    Added more user and host routes for the side bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,15 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Host from './Pages/hostDashboard'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
-import Guest from './Pages/GuestDashBoard'
-import Register from './Pages/Register'
+import SignUp from './Pages/SignUp'
+import Property from './Pages/RegisterProperty'
+import GuestDashboard from './Pages/GuestDashBoard'
+import HostDashboard from './Pages/HostDashboard'
+import RegisterVehicle from './Pages/RegisterVehicle'
+import GuestUserSettings from './Pages/GuestUserSettings'
+import HostUserSettings from './Pages/HostUserSettings'
 
 
 
@@ -18,10 +22,14 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/host" element={<Host />} />
-        <Route path="/guest" element={<Guest />} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/hostdashboard" element={<HostDashboard />} />
+        <Route path="/guestdashboard" element={<GuestDashboard />} />
         <Route path="/home" element={<Home/>} />
+        <Route path="/hostusersettings" element={<HostUserSettings/>} />
+        <Route path="/property" element={<Property/>} />
+        <Route path="/registervehicle" element={<RegisterVehicle/>} />
+        <Route path="/guestusersettings" element={<GuestUserSettings/>} />
       </Routes>
     </BrowserRouter>
   )
