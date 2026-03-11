a8b45d811abaaab930fc402c17028fbd54197213
Legends-Land <caandrews18@gmail.com>
2026-03-10 20:52:37 -0400

    Created register vehicle form for guest users

---
+++ client/src/Pages/RegisterVehicle.jsx
@@ -0,0 +1,51 @@
+import GuestSideBar from "../Components/GuestSideBar"
+
+const RegisterVehicle = () => {
+
+  return(
+    <>
+    
+    <h2>Register Vehicle</h2>
+
+    <label> Model
+      <input
+      type="text"
+      ></input>
+    </label>
+
+    <label> Make
+      <input
+      type="texg"
+      ></input>
+    </label>
+
+    <label>Year
+      <input
+      type="number"
+      ></input>
+    </label>
+
+    <label> Color
+      <input
+      type="color"
+      ></input>
+    </label>
+
+    <label> License No. 
+      <input
+      type="number"
+      ></input>
+    </label>
+
+    <label> Body Type
+      <input
+      type="text"
+      ></input>
+    </label>
+    
+    <GuestSideBar/>
+    </>
+  )
+}
+
+export default RegisterVehicle;
