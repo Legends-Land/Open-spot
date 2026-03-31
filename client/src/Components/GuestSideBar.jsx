ec36e65b3d83c4f63482e337f96fa3d6d8d28be0
Legends-Land <caandrews18@gmail.com>
2026-03-31 17:57:32 -0400

    Updated side bar with no onclick. Just navigation

--- client/src/Components/GuestSideBar.jsx
+++ client/src/Components/GuestSideBar.jsx
@@ -2,27 +2,27 @@ import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 
 const GuestSideBar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  
 
   return (
     <div className="sidebar-guest">
       
       {/* Hamburger button - only shows on mobile */}
-      <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+      {/* <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
         <span className={isOpen ? "bar bar1-open" : "bar"} />
         <span className={isOpen ? "bar bar2-open" : "bar"} />
         <span className={isOpen ? "bar bar3-open" : "bar"} />
-      </button>
+      </button> */}
 
       {/* Nav links */}
       <aside>
-        <nav className={isOpen ? "nav-links open" : "nav-links"}>
-          <Link to="/home" onClick={() => setIsOpen(false)}>Home</Link>
-          <Link to="/guestdashboard" onClick={() => setIsOpen(false)}>Guest Dashboard</Link>
-          <Link to="/guestusersettings" onClick={() => setIsOpen(false)}>Guest Settings</Link>
-          <Link to="/registervehicle" onClick={() => setIsOpen(false)}>Register Vehicle</Link>
-          <Link to="/" onClick={() => setIsOpen(false)}>Log Out</Link>
-          <Link to="/hostdashboard" onClick={() => setIsOpen(false)}>Host Dashboard</Link>
+        <nav >
+          <Link to="/home" >Home</Link>
+          <Link to="/guestdashboard" >Guest Dashboard</Link>
+          <Link to="/guestusersettings" >Guest Settings</Link>
+          <Link to="/registervehicle" >Register Vehicle</Link>
+          <Link to="/">Log Out</Link>
+          <Link to="/hostdashboard" >Host Dashboard</Link>
         </nav>
       </aside>
 
